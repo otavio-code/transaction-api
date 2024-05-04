dccdc2201106af154fda602a2eaba3c7361ea9c8
Otavio Andrade <otavio.sto@gmail.com>
2024-05-04 11:34:09 -0300

    feat: initalizer spring realizado

---
+++ src/test/java/br/com/fiap/fastfood/transactionapi/TransactionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fastfood.transactionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
